78556a7b89067c2d33f801ac29fb464decf9faa9
RodrigoMaga <rgn.nunnes@gmail.com>
2023-08-14 13:40:08 -0300

    ssolucao opcao invalida

--- src/main/java/br/com/ragnainterprise/services/JankenponService.java
+++ src/main/java/br/com/ragnainterprise/services/JankenponService.java
@@ -8,10 +8,13 @@ public class JankenponService {
 
     Player player1 = askPlayerInput()[0];
     Player player2 = askPlayerInput()[0];
+    String winner;
 
     public void gamePlay() {
 
         findWinner();
+
+        System.out.println(winner + " Wins!");
 
     }
 
@@ -21,24 +24,19 @@ public class JankenponService {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Informe seu nome: ");
         player.name = scanner.next();
-        System.out.printf("Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
-        System.out.print("Informe sua escolha: ");
-        player.hand = scanner.nextInt();
-        if (player.hand < 1 || player.hand > 3){
-            System.out.println("Escolha uma opção válida!");
-        }
+        player.hand = getHand();
 
-        return new Player[] {player};
+        return new Player[]{player};
     }
     private void drawSolver() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
+        System.out.printf(player1.name + " Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
         System.out.print("Informe sua escolha: ");
 
         player1.hand = scanner.nextInt();
 
-        System.out.printf("Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
+        System.out.printf(player2.name + " Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
         System.out.print("Informe sua escolha: ");
 
         player2.hand = scanner.nextInt();
@@ -53,39 +51,57 @@ public class JankenponService {
 
     private void findWinner() {
 
+
+
         if (player1.hand == player2.hand) {
             System.out.println("Empate!");
-            drawSolver(); //todo resolver questão do empate
+            drawSolver();
         }
 
         if (player1.hand == 1 && player2.hand == 2) {
-            System.out.println(player2.name + " Wins!");
+            winner = player2.name;
             return;
         }
 
         if (player1.hand == 2 && player2.hand == 1) {
-            System.out.println(player1.name + " Wins!");
+            winner = player1.name;
             return;
         }
 
         if (player1.hand == 1 && player2.hand == 3) {
-            System.out.println(player1.name + " Wins!");
+            winner = player1.name;
             return;
         }
 
         if (player1.hand == 3 && player2.hand == 1) {
-            System.out.println(player2.name + " Wins!");
+            winner = player2.name;
             return;
         }
 
         if (player1.hand == 2 && player2.hand == 3) {
-            System.out.println(player2.name + " Wins!");
+            winner = player2.name;
             return;
         }
 
         if (player1.hand == 3 && player2.hand == 2) {
-            System.out.println(player1.name + " Wins!");
+            winner = player1.name;
         }
     }
 
+    private int getHand() {
+        Scanner scanner = new Scanner(System.in);
+        Player player = new Player();
+
+        System.out.printf("Opções disponíveis: %n 1 - Pedra %n 2 - Papel %n 3 - Tesoura%n");
+        System.out.print("Informe sua escolha: ");
+        player.hand = scanner.nextInt();
+
+        while (player.hand < 1 || player.hand > 3){
+            System.out.print("Escolha uma opção válida: ");
+            player.hand = scanner.nextInt();
+            //gethand();
+            //quando tava chamando o método ele ficava reatribuindo o player aí o nome ficava null
+        }
+        return player.hand;
+    }
 }
